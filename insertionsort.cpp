fd3d69164fe6a5fc70e2b91bf99f2ac3373f9570
jordanbarile <jordan.barile.love@gmail.com>
2025-11-19 21:53:53 -0500

    Update insertionsort.cpp

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,19 +1,17 @@
-/*
- * insertionsort.cpp
- *
- *  Created on: Nov 18, 2025
- *      Author: ece-lab-206
- */
+//Jordan Barile
+//ECE 2161-001
+// A program to use insertion sort on both small and large arrays of numbers
+
 #include "myheader.h"
 
 void insertionSort(long data[] , int n) {
-    for (int i = 1; i < n; i++) {
-        long tmp = data[i];
-        int j = i;
-        while (j > 0 && tmp < data[j - 1]) {
+    for (int i = 1; i < n; i++) { //start from the second element
+        long tmp = data[i]; // value to insert
+        int j = i; // scan left
+        while (j > 0 && tmp < data[j - 1]) { // shift elements greater than tmp to the right 
             data[j] = data[j - 1];
             j--;
         }
-        data[j] = tmp;
+        data[j] = tmp; // put everything in the right spot
     }
 }

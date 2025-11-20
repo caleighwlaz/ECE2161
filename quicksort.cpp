341e64f11f5ad4800111c8b2cbea144a05f46051
jordanbarile <jordan.barile.love@gmail.com>
2025-11-19 21:51:19 -0500

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,21 +1,21 @@
-/*
- * quicksort.cpp
- *
- *  Created on: Nov 18, 2025
- *      Author: ece-lab-206
- */
+// Jordan Barile
+// Lab 5, ECE 2161-001
+// A program to use quick sort in order to sort both short and long arrays of numbers
 
 #include "myheader.h"
 
 void quickSort(long data[], long left, long right) {
-    if (left >= right)
+    if (left >= right) //base case
         return;
 
+    //pivot is the middle element
     long bound = data[(left + right) / 2];
 
     long i = left, j = right;
     while (i <= j) {
+        //move i forwards until a number greater than i is found
         while (data[i] < bound) i++;
+        //move j backwards until a number less than j is found
         while (data[j] > bound) j--;
 
         if (i <= j) {
@@ -25,8 +25,8 @@ void quickSort(long data[], long left, long right) {
         }
     }
 
-    if (left < j)
+    if (left < j) //sort the left
         quickSort(data, left, j);
-    if (i < right)
+    if (i < right) //sort the right
         quickSort(data, i, right);
 }

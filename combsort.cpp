911e4ce1f1586af532686735ab910cd12e61d525
jpetrelli29 <jpetrelli29@gmail.com>
2025-11-19 21:26:42 -0500

    Update combsort.cpp comments

--- combsort.cpp
+++ combsort.cpp
@@ -1,25 +1,22 @@
-/*
- * combsort.cpp
- *
- *  Created on: Nov 18, 2025
- *      Author: ece-lab-206
- */
+// Julianna Petrelli
+// Lab #5, ECE 2161-001
+// The following code preforms a comb sort algorithm on a given data set. 
 #include "myheader.h"
 
-void combsort(long data[], const int n) {
+void combsort(long data[], const int n) {                //function declaration and definition
 	int step = n, j, k;
 	while ((step = int(step/1.3)) > 1)                   //phase 1
-		for (j = n - 1; j >= step; j--) {
+		for (j = n - 1; j >= step; j--) {                //processes data from right to left, sorting larger numbers first
 			k = j - step;
 			if (data[j] < data[k])
-				swap(data[j], data[k]);
+				swap(data[j], data[k]);                  //swaps numbers if element j is less than element k, so that element j appears first
 		}
 	bool again = true;
 	for (int i = 0; i < n - 1 && again; i++) {           //phase 2
 		for (j = n - 1, again = false; j > i; --j) {
 			if (data[j] < data[j-1]) {
 				swap(data[j], data[j-1]);
-				again = true;
+				again = true;                            //adds a flag to discontinue processing after no swaps were performed (data has been sorted)
 			}
 		}
 	}

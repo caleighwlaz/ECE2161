6836b94928aacd67a48bd3f4a096c86cbd58accd
Caleigh W. <153221850+caleighwlaz@users.noreply.github.com>
2025-11-21 11:02:13 -0500

    Update myheader.h comments

--- myheader.h
+++ myheader.h
@@ -5,21 +5,21 @@
 
 using namespace std;
 
-void	printmenu();
-void	printarray(long data[], long size);
-int		readfile(string infilename, long d[]);
-int		writefile(long data[], long size, string outfilename);
+void	printmenu(); //prints options to user
+void	printarray(long data[], long size);  //prints data array
+int		readfile(string infilename, long d[]); //reads input file
+int		writefile(long data[], long size, string outfilename); //writes outout file
 
-void	swap(long& x, long& y);
+void	swap(long& x, long& y); //swaps two variable values
 
-void combsort(long data[], const int n);
+void combsort(long data[], const int n);  //sorts wData using Comb Sort
 
-void insertionSort(long data[], int n);
+void insertionSort(long data[], int n);  //sorts wData using Insertion Sort
 
-void swap_sel(long &a, long &b);
+void swap_sel(long &a, long &b); //another swap function that swaps two variable values
 
-void selectionsort(long data[], int n);
-void mergesort(long data[], long first, long last);
-void merge(long array[], long first, long last);
-void quickSort(long data[], long left, long right);
-void shellsort(long data[], int n);
+void selectionsort(long data[], int n); //sorts wData using Selection Sort
+void mergesort(long data[], long first, long last); //sorts wData using Merge Sort
+void merge(long array[], long first, long last); //used for Merge Sort
+void quickSort(long data[], long left, long right);  //sorts wData using Quick Sort
+void shellsort(long data[], int n); //sorts wData using Shell Sort
